79f1e860b82b18de9e1d95d825dbcbd78182f006
Joachim Stroh <joachim@sceenius.com>
2018-10-09 10:35:10 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import App from "./App";
+
+Vue.config.productionTip = false;
+
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+import "./custom.scss";
+
+Vue.use(VueMaterial);
+
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>"
+});

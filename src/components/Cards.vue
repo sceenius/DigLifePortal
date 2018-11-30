e45ad9948f02646ce5b3ef4804ffe5b0966fbe3e
Joachim Stroh <joachim@sceenius.com>
2018-11-30 17:45:05 -0500

    Cards tuning

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,24 +1,57 @@
 <template>
   <div>
-    <md-card md-with-hover>
-      <md-card-header>
-        <md-card-header-text>
-          <div class="md-title">Media card</div>
-          <div class="md-subhead">Medium size</div>
-        </md-card-header-text>
+    <md-card
+      md-with-hover
+      v-for="(card, index) in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+      :key="index"
+    >
+      <md-card-area md-inset>
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">Research Topic</div>
+            <div class="md-subhead">Self-souverign Identity</div>
+            <md-chip>Active</md-chip>
+          </md-card-header-text>
 
-        <md-card-media md-medium>
+          <md-card-media md-medium>
+            <img
+              src="https://ledger.diglife.coop/images/cards/card_chemistry.svg"
+              alt="People"
+            />
+          </md-card-media>
+        </md-card-header>
+
+        <md-card-actions>
+          <md-button>Action</md-button>
+          <md-button>Action</md-button>
+        </md-card-actions>
+      </md-card-area>
+      <div class="md-card-footer">
+        <md-avatar>
           <img
-            src="https://vuematerial.io/assets/examples/card-weather.png"
-            alt="People"
+            src="https://ledger.diglife.coop/images/avatars/avatar_jimscarver.png"
+            alt="Avatar"
           />
-        </md-card-media>
-      </md-card-header>
-
-      <md-card-actions>
-        <md-button>Action</md-button>
-        <md-button>Action</md-button>
-      </md-card-actions>
+        </md-avatar>
+        <md-avatar>
+          <img
+            src="https://ledger.diglife.coop/images/avatars/avatar_christina.png"
+            alt="Avatar"
+          />
+        </md-avatar>
+        <md-avatar>
+          <img
+            src="https://ledger.diglife.coop/images/avatars/avatar_joachim.png"
+            alt="Avatar"
+          />
+        </md-avatar>
+        <md-avatar>
+          <img
+            src="https://ledger.diglife.coop/images/avatars/avatar_graham.png"
+            alt="Avatar"
+          />
+        </md-avatar>
+      </div>
     </md-card>
   </div>
 </template>
@@ -51,9 +84,35 @@ export default {
 
 <style>
 .md-card {
-  width: 320px;
-  margin: 24px;
+  width: 360px;
+  margin: 14px;
   display: inline-block;
   vertical-align: top;
+}
+.md-card .md-title {
+  font-weight: normal !important;
+  font-size: 1.8em !important;
+  line-height: 1em;
+  margin-bottom: 5px;
+  width: 200px !important;
+}
+.md-card-footer {
+  margin-left: 10px;
+}
+.md-card .md-avatar {
+  margin: 10px 0px 10px 4px;
+  border: 1px #ccc solid;
+}
+.md-card .md-chip {
+  background-color: red;
+  color: white;
+  margin: 10px 0 0 0;
+  padding: 0px 30px 0px 30px;
+  font-size: 1em;
+  font-weight: bold;
+}
+
+.md-card-media img {
+  position: absolute !important;
 }
 </style>
